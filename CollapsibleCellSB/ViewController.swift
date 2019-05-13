831ced7f0cd8d3c1cfe7705d87bc868dfcab5844
Josh R <seventytwoapp@gmail.com>
2019-05-13 11:23:14 -0500

    Initial Commit

---
+++ CollapsibleCellSB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollapsibleCellSB
+//
+//  Created by Josh R on 5/13/19.
+//  Copyright © 2019 Josh R. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
